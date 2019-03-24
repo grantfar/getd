ec077e575530d33e174d3ded42f7af5c22a5849e
jsziede <jsziede@yahoo.com>
2019-03-24 18:47:12 -0400

    Fix outdated header file.

--- requestHandlers.h
+++ requestHandlers.h
@@ -10,9 +10,13 @@
 
 #include "requests.h"
 
-int MessageType0Handler(MessageType0 * messageType0, State * state);
+int MessageType0Handler(MessageType0 * messageType0, State * state, void * outMessage);
+int MessageType2Handler(MessageType2 * messageType2);
 int MessageType3Handler(MessageType3 * messageType3, State * state, void * outMessage);
 int MessageType6Handler(MessageType6 * messageType6, State * state, void * outMessage);
 int MessageOtherHandler(char * message, unsigned char type , State * state, void * outMessage);
-MessageType2 MessageType2Builder(char * errorMsg);
+int MessageType2Builder(MessageType2 * messageType2,char *errorMsg);
+int MessegeType4Builder(MessageType4 * out,State * state);
+int MessegeType5Builder(MessageType5 * out,State * state);
+int MessageType1Builder(MessageType1 * out, State * state);
 #endif //GETD_REQUESTHANDLERS_H
